00dc7c5e56329c4fa5c91c043ec58e7e6812cbc1
Bogdan <69055164+BKarpow@users.noreply.github.com>
2020-12-04 19:54:20 +0300

    Update ConfigMySql.php

--- lib/class/mysql/ConfigMySql.php
+++ lib/class/mysql/ConfigMySql.php
@@ -8,9 +8,9 @@
 class ConfigMySql
 {
 	protected $host = 'localhost';
-	protected $user = 'dexby101_chat';
-	protected $password = '+TZ4ue-AsjqK';
-	protected $base = 'dexby101_chat';
+	protected $user = '';
+	protected $password = '';
+	protected $base = '';
 	protected $charset = 'utf8';
 	protected string $format_date = 'Y-m-d H:i:s';
 	protected string $timezone = 'Kiev/Europe';
